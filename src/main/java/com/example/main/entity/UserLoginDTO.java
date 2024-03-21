bf73e843a688f685fbb3bc46331373f608b6d964
praveenece2704 <praveenkumarthesis@gmail.com>
2024-03-22 01:18:53 +0530

    Backend code

---
+++ src/main/java/com/example/main/entity/UserLoginDTO.java
@@ -0,0 +1,13 @@
+package com.example.main.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+	private String email;
+	private String password;
+}
